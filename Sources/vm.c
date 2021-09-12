1e08d693c11eb1ea20c130f86f7f896e962834dd
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-12 18:11:15 -0500

    Fixed some build time errors.

--- Sources/vm.c
+++ Sources/vm.c
@@ -49,11 +49,15 @@ static Value peek(int distance) {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
+#define BINARY_OP(valueType, op) \
   do { \
-    double b = pop(); \
-    double a = pop(); \
-    push(a op b); \
+    if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+      runtimeError("Operands must be numbers."); \
+      return INTERPRET_RUNTIME_ERROR; \
+    } \
+    double b = AS_NUMBER(pop()); \
+    double a = AS_NUMBER(pop()); \
+    push(valueType(a op b)); \
   } while (false)
 
 
@@ -85,10 +89,10 @@ static InterpretResult run() {
         push(constant);
         break;
       }
-      case OP_ADD:        BINARY_OP(+); break;
-      case OP_SUBSTRACT:  BINARY_OP(-); break;
-      case OP_MULTIPLY:   BINARY_OP(*); break;
-      case OP_DIVIDE:     BINARY_OP(/); break;
+      case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
+      case OP_SUBSTRACT:  BINARY_OP(NUMBER_VAL, -); break;
+      case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
+      case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
       case OP_NEGATE: 
         if (!IS_NUMBER(peek(0))) {
           runtimeError("Operand must be a number.");
