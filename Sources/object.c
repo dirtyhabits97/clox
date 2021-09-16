4013a9a51a02b1de4e61354cfab5edee64efa570
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-15 21:10:37 -0500

    Finished implementing hashtables edge cases and string comparison

--- Sources/object.c
+++ Sources/object.c
@@ -3,6 +3,7 @@
 
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -32,6 +33,7 @@ static ObjString* allocateString(
   string->length = length;
   string->chars = chars;
   string->hash = hash;
+  tableSet(&vm.strings, string, NIL_VAL);
   return string;
 }
 
@@ -47,11 +49,22 @@ static uint32_t hashString(const char* key, int length) {
 
 ObjString* takeString(char* chars, int length) {
   uint32_t hash = hashString(chars, length);
+  ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+
+  if (interned != NULL) {
+    FREE_ARRAY(char, chars, length + 1);
+    return interned;
+  }
+
   return allocateString(chars, length, hash);
 }
 
 ObjString* copyString(const char* chars, int length) {
   uint32_t hash = hashString(chars, length);
+  ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+
+  if (interned != NULL) return interned;
+
   char* heapChars = ALLOCATE(char, length + 1);
   memcpy(heapChars, chars, length);
   heapChars[length] = '\0';
