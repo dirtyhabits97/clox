15ce0f2187aa668f63befebca7a3872c0430c685
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-17 10:26:04 -0500

    Variable in its own initializer edge case;

--- Sources/compiler.c
+++ Sources/compiler.c
@@ -191,6 +191,9 @@ static int resolveLocal(Compiler* compiler, Token* name) {
   for (int i = compiler->localCount - 1; i >= 0; i--) {
     Local* local = &compiler->locals[i];
     if (identifiersEqual(name, &local->name)) {
+      if (local->depth == -1) {
+        error("Can't read local variable in its own initializer.");
+      }
       return i;
     }
   }
@@ -206,7 +209,7 @@ static void addLocal(Token name) {
   }
   Local* local = &current->locals[current->localCount++];
   local->name = name;
-  local->depth = current->scopeDepth;
+  local->depth = -1;
 }
 
 static void declareVariable() {
@@ -384,8 +387,13 @@ static uint8_t parseVariable(const char* errorMessage) {
   return identifierConstant(&parser.previous);
 }
 
+static void markInitialized() {
+  current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
   if (current->scopeDepth > 0) {
+    markInitialized();
     return;
   }
 
