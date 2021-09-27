180dc755407e015f87ac7bad11be2a1cbfed4050
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-26 22:00:47 -0500

    Self-adjusting heap.

--- Sources/memory.c
+++ Sources/memory.c
@@ -10,15 +10,22 @@
 #include "debug.h"
 #endif
 
+#define GC_HEAP_GROW_FACTOR 2
+
 void* reallocate(
   void* pointer,
   size_t oldSize,
   size_t newSize
 ) {
+  vm.bytesAllocated += newSize - oldSize;
   if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
     collectGarbage();
 #endif
+
+    if (vm.bytesAllocated > vm.nextGC) {
+      collectGarbage();
+    }
   }
   // When new size is zero,
   // we dealloc
@@ -183,6 +190,7 @@ static void sweep() {
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
+  size_t before = vm.bytesAllocated;
 #endif
 
   markRoots();
@@ -190,8 +198,14 @@ void collectGarbage() {
   traceReferences();
   sweep();
 
+  vm.nextGC = vm.bytesAllocated * GC_HEAP_GROW_FACTOR;
+
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
+  printf(
+    "   collected %zu bytes (from %zu to %zu) next at %zu\n",
+    before - vm.bytesAllocated, before, vm.bytesAllocated, vm.nextGC
+  );
 #endif
 }
 
