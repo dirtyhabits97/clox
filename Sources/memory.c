b7d1b06b75dae84e30c27a0b7b122386ca6efab3
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-26 22:15:34 -0500

    Class objects.

--- Sources/memory.c
+++ Sources/memory.c
@@ -81,6 +81,11 @@ static void blackenObject(Obj* object) {
 #endif
 
   switch (object->type) {
+    case OBJ_CLASS: {
+      ObjClass* klass = (ObjClass*)object;
+      markObject((Obj*)klass->name);
+      break;
+    }
     case OBJ_CLOSURE: {
       ObjClosure* closure = (ObjClosure*)object;
       markObject((Obj*)closure->function);
@@ -135,6 +140,10 @@ static void freeObject(Obj* object) {
   printf("%p free type %d\n", (void*)object, object->type);
 #endif
   switch (object->type) {
+    case OBJ_CLASS: {
+      FREE(ObjClass, object);
+      break;
+    }
     case OBJ_NATIVE:
       FREE(ObjNative, object);
       break;
